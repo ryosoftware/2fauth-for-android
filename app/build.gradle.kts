40eff74250a9eb8915474526eff5a69e6543ba3f
Roberto P. Rubio <outtakes_handgun090@familiapm.com>
2025-03-13 06:48:16 +0100

    Bump to 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.twofauth.android"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
 
     buildTypes {
